beed2755aaddd5f8e9ed4b94d8e6e8a139b99b32
Lucifer Ronaldo <108746571+luciferro01@users.noreply.github.com>
2023-04-08 19:25:30 +0530

    Changing setState({}) to GetX State Management

---
+++ lib/onboarding_screen/data/item_data.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/image_strings.dart';
+import '../models/item_data.dart';
+
+List<ItemData> data = [
+  ItemData(
+      image: onbScreen1,
+      bgColor: Colors.amber,
+      descriptionText: 'A one stop solution of all the apps you need',
+      headingText: 'Welome to the Super App'),
+  ItemData(
+      image: onbScreen2,
+      bgColor: Colors.green,
+      descriptionText: 'Conatains all the Apps you need in your daily life',
+      headingText: 'Productive'),
+  ItemData(
+      image: onbScreen3,
+      bgColor: Colors.yellow,
+      descriptionText: 'So are you ready to deep dive into this app',
+      headingText: 'Dive into it'),
+];
